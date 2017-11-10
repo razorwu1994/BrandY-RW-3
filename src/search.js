ec42b82a3f0cf3bd11d8e7c8e0ff716a95b5fa73
bjyoung <bjyoung20591@gmail.com>
2017-11-10 14:37:22 -0500

    Added skeleton for search.js

---
+++ src/search.js
@@ -0,0 +1,17 @@
+function uniform_cost_search(){
+    // Get data
+    // Run search
+    // Return the path (1D array)
+}
+
+function heuristic_search(){
+    // Get data
+    // Run search
+    // Return the path (1D array)
+}
+
+function weighted_heuristic_search(){
+    // Get data
+    // Run search
+    // Return the path (1D array)
+}
