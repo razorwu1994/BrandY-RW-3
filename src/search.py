a5777d1fd5fc183456cf190daa9c1d1ce455c288
bjyoung <bjyoung20591@gmail.com>
2017-11-12 15:15:33 -0500

    Added some skeleton code

---
+++ src/search.py
@@ -0,0 +1,28 @@
+import heapq
+
+def uniform_cost_search(start, goal, grid):
+    """
+    Do uniform cost search on the given grid, start and goal
+
+    Parameters:
+    start = coordinates of the start position
+    goal = coordinates of the goal position
+    grid = entire 160x120 grid map
+    """
+
+    
+    " Run search "
+    " Return the path (1D array) "
+}
+
+def function heuristic_search(){
+    " Get data "
+    " Run search "
+    " Return the path (1D array) "
+}
+
+function weighted_heuristic_search(){
+    " Get data "
+    " Run search "
+    " Return the path (1D array) "
+}
