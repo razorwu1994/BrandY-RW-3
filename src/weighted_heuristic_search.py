7510122ae12aa7834a8f2b47e1dc1ed6847d4073
bjyoung <bjyoung20591@gmail.com>
2017-11-18 19:53:20 -0500

    Added Weighted A* Class

---
+++ src/weighted_heuristic_search.py
@@ -0,0 +1,27 @@
+from heuristic_search import HeuristicSearch
+
+class WeightedHeuristicSearch(HeuristicSearch):
+    """
+    Class that performs heuristic search on a 160x120 grid.
+    Use search to find a path of coordinates to follow from start to finish.
+
+    Attributes:
+    grid = 160x120 grid of cells that represent the map
+    """
+
+    def __init__(self, grid, heuristic, weight):
+        HeuristicSearch.__init__(self, grid, heuristic)
+        self.weight = weight
+
+    def apply_heuristic(self, cell, goal):
+        """
+        Calculate and set the heuristic for a cell
+
+        Parameters:
+        cell = target cell
+        goal = coordinates of goal cell
+
+        Returns: h value for the cell
+        """
+        cell.h = self.weight * self.heuristic(cell, goal)
+        return cell.h
