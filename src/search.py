d89d286f2ae07198d0b16deaba67f5f6c1bcb963
razorwu1994 <razor.wu1994@gmail.com>
2017-11-23 13:09:41 -0500

    toggle off the output for experimental

--- src/search.py
+++ src/search.py
@@ -141,11 +141,12 @@ if __name__ == "__main__":
         t2 = datetime.now()
         delta = t2 - t1
         print 'Time (In seconds):{}'.format(delta.total_seconds())
-        # Write path to a file
-        f = open('experimental.csv','a+')
-        output = file_name+","+search_type+","+heuristic_type+","+str(weight)+","+str(len(path))+","+str(num_nodes_expanded)+","+str(delta.total_seconds())+"\n"
-        f.write(output)
-        f.close()
+
+        # Write path to a file,toggle this off when doing the demo
+        # f = open('experimental.csv','a+')
+        # output = file_name+","+search_type+","+heuristic_type+","+str(weight)+","+str(len(path))+","+str(num_nodes_expanded)+","+str(delta.total_seconds())+"\n"
+        # f.write(output)
+        # f.close()
 
         # Write f,g,h data to a file in format[(f, g, h), (f, g, h), ..., (f, g, h)]
         f = open('extra.txt', 'w')
